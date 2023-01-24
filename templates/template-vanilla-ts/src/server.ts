e79af156b40831ca709d1f2387f69df4b816bd90
Olivier Louvignes <olivier@mgcrea.io>
2023-01-24 16:23:35 +0100

    feat(template-vanilla-ts): bump deps and minor tweaks

--- templates/template-vanilla-ts/src/server.ts
+++ templates/template-vanilla-ts/src/server.ts
@@ -1,5 +1,5 @@
 import http, { RequestListener } from "node:http";
-import { name, version } from "./config";
+import { PACKAGE_NAME, PACKAGE_VERSION } from "./config";
 
 export type ServerOptions = {
   port?: number;
@@ -9,31 +9,37 @@ export const createServer = async (options: ServerOptions = {}) => {
   const port = options.port ?? 3000;
 
   const handler: RequestListener = (req, res) => {
-    console.log(`[${name}] incoming ${req.method} request on url="${req.url}"`);
+    console.log(`Incoming ${req.method} request on url="${req.url}"`);
     res.writeHead(200, { "Content-Type": "application/json; charset=utf-8" });
     res.end(
       JSON.stringify({
-        name,
-        version,
+        name: PACKAGE_NAME,
+        version: PACKAGE_VERSION,
         data: "Hello 🌎!",
       })
     );
   };
 
+  const server = http.createServer(handler);
+
   const start = async (listen: boolean) => {
     try {
-      const server = await http.createServer(handler);
       if (!listen) {
         return server;
       }
-      await server.listen(port);
-      console.log(`[${name}] listening on port ${port} ...`);
+      await new Promise((resolve, reject) => {
+        server.listen(port).once("listening", resolve).once("error", reject);
+      });
+      console.log(`Server listening at http://127.0.0.1:${port}`);
       return server;
     } catch (err) {
       console.error(err);
       process.exit(1);
     }
   };
+  const stop = () => {
+    server.close();
+  };
 
-  return { start, handler };
+  return { start, stop, handler };
 };
