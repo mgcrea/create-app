08e09ff07f6ecdd6af6b6d73a26154bf4c0a10f2
Olivier Louvignes <olivier@mgcrea.io>
2023-01-24 16:23:35 +0100

    feat(template-vanilla-ts): bump deps and minor tweaks

--- templates/template-vanilla-ts/src/config.ts
+++ templates/template-vanilla-ts/src/config.ts
@@ -1,4 +1,12 @@
 import { createRequire } from "node:module";
-const require = createRequire(import.meta.url);
 
-export const { name, version } = require("../package.json") as { name: string; version: string };
+// Package
+const require = createRequire(import.meta.url);
+export const { name: PACKAGE_NAME, version: PACKAGE_VERSION } = require("../package.json") as {
+  name: string;
+  version: string;
+};
+
+// Environment
+export const NODE_ENV = process.env["NODE_ENV"] ?? "development";
+export const [IS_DEV, IS_PROD] = [NODE_ENV === "development", NODE_ENV === "production"];
