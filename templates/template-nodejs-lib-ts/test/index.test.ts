1970103adf8fe0872ccd182a404805129ebb43e3
Olivier Louvignes <olivier@mgcrea.io>
2023-01-31 15:32:31 +0100

    feat(template): add template-nodejs-lib-ts

---
+++ templates/template-nodejs-lib-ts/test/index.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, test } from "vitest";
+import * as module from "..";
+
+describe("module", () => {
+  test("exports", () => {
+    expect(Object.keys(module)).toMatchInlineSnapshot(`
+      [
+        "default",
+      ]
+    `);
+  });
+});
