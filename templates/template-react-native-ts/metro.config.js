d9de8da2c001c3c92debc1dd86ccecce29e837c4
Olivier Louvignes <olivier@mgcrea.io>
2023-03-20 17:50:22 +0100

    feat(templates): add vitest.config.ts for backend project to properly support tsconfig paths

---
+++ templates/template-react-native-ts/metro.config.js
@@ -0,0 +1,11 @@
+/** @type {import('metro-config').InputConfigT} */
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
