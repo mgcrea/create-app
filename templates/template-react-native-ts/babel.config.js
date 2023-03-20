74be7d1d34b2599a75cef4b685a51202c6584a0a
Olivier Louvignes <olivier@mgcrea.io>
2023-03-20 17:50:22 +0100

    feat(templates): add vitest.config.ts for backend project to properly support tsconfig paths

---
+++ templates/template-react-native-ts/babel.config.js
@@ -0,0 +1,15 @@
+/** @type {import('@babel/core').TransformOptions} */
+module.exports = {
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          src: "./src",
+        },
+      },
+    ],
+    "react-native-reanimated/plugin",
+  ],
+};
