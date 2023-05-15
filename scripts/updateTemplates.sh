6b8dc8317b16364ad3b5a08963f5110569c50b0c
Olivier Louvignes <olivier@mgcrea.io>
2023-05-15 22:42:51 +0200

    chore(ci): improve template testing

---
+++ scripts/updateTemplates.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e # terminate on first failure
+
+ROOT_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+${ROOT_DIR}/runTemplates.sh "ncu -f @types/node -t minor -u; ncu -x @types/node -u; rm -rf pnpm-lock.yaml node_modules; pnpm install"
