373e53073dce4d558bddb4c762ecc41960e6de6d
Olivier Louvignes <olivier@mgcrea.io>
2023-06-12 10:49:30 +0200

    chore(npm): bump deps, update eslint configs

--- scripts/updateTemplates.sh
+++ scripts/updateTemplates.sh
@@ -4,4 +4,4 @@ set -e # terminate on first failure
 
 ROOT_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-${ROOT_DIR}/runTemplates.sh "ncu -f @types/node -t minor -u; ncu -x @types/node -u; rm -rf pnpm-lock.yaml node_modules; pnpm install"
+${ROOT_DIR}/runTemplates.sh "ncu -f @types/node -t minor -u; ncu -x @types/node -u; rm -rf pnpm-lock.yaml node_modules; pnpm upgrade"
