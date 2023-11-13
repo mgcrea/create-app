1cb2ad6e182e45e10ad8197780c28801b88f116d
Olivier Louvignes <olivier@mgcrea.io>
2023-11-13 23:37:28 +0100

    chore(npm): bump deps

--- scripts/updateTemplates.sh
+++ scripts/updateTemplates.sh
@@ -4,4 +4,5 @@ set -e # terminate on first failure
 
 ROOT_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-${ROOT_DIR}/runTemplates.sh "ncu -f @types/node -t minor -u; ncu -x @types/node -u; rm -rf pnpm-lock.yaml node_modules; pnpm upgrade"
+ncu -u
+${ROOT_DIR}/runTemplates.sh "ncu -u; pnpm upgrade"
